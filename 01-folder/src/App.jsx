a7f1dd81c54dfc469cfed86a0d24a5b5e4a28d94
Abdul Razzaq <shadsaiddiqui@gmail.com>
2025-11-10 22:08:45 +0530

    01-folder-done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+
+    <>
+      <div id='parent'>
+        <h1 id='child1'>App</h1>
+        <h3 id='child2'>Another Line</h3>
+      </div>
+
+      <div id='relative'>
+
+      </div>
+    
+    </>
+
+  )
+}
+
+export default App
